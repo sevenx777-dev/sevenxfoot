59a15b4013a04ab89cf4dae58b33162e6af3ff71
sevenx777-dev <sevenxoficial777@gmail.com>
2025-06-30 18:54:57 -0300

    Create main.tsx

---
+++ app/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Importa o seu componente App principal
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
