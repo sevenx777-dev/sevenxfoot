adbaa2b6db54226cb8a5bf35146136daacbf7832
sevenx777 <sevenxoficial777@gmail.com>
2025-06-30 08:40:39 -0300

    Corrige import do CSS e configura Tailwind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import FootballManager from '../football_manager';
 import { useWindowDimensions, View, Text } from 'react-native';
 
+
 export default function Index() {
   const { width, height } = useWindowDimensions();
   const isPortrait = height >= width;
